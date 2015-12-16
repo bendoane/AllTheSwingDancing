a65cb7a06821719af95cddaaa49b6a58728a278b
bendoane <ben.doane@gmail.com>
2015-12-16 09:55:40 -0500

    misc.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-example_event1 = Event.create({name:"This Example Event!",organizer:"Naptown Stomp",date:"2015-12-17",location:"Indianapolis, Indiana",price:"30.00",event_type:"workshop",dance_style:"lindy"})
-example_event2 = Event.create({name:"Dayton Swing Smackdown Example",organizer:"Naptown Stomp",date:"2015-4-13",location:"Indianapolis, Indiana",price:30.00,event_type:"workshop",dance_style:"lindy"})
-example_event3 = Event.create({name:"Southern Swing Challenge Example",organizer:"Naptown Stomp",date:"2015-9-23",location:"Indianapolis, Indiana",price:30.00,event_type:"workshop",dance_style:"lindy"})
-example_event4 = Event.create({name:"Lindypalooza Whatchakadooza Example",organizer:"Naptown Stomp",date:"1989-11-29",location:"Indianapolis, Indiana",price:30.00,event_type:"workshop",dance_style:"lindy"})
+example_event1 = Event.create({name:"This Example Event!",organizer:"Naptown Stomp",date:"2015-12-17",location:"Indianapolis, Indiana",price:"30.00",event_type:"Workshop",dance_style:"Lindy"})
+example_event2 = Event.create({name:"Dayton Swing Smackdown Example",organizer:"Naptown Stomp",date:"2015-4-13",location:"Indianapolis, Indiana",price:30.00,event_type:"Workshop",dance_style:"Lindy"})
+example_event3 = Event.create({name:"Southern Swing Challenge Example",organizer:"Naptown Stomp",date:"2015-9-23",location:"Indianapolis, Indiana",price:30.00,event_type:"Workshop",dance_style:"Lindy"})
+example_event4 = Event.create({name:"Lindypalooza Whatchakadooza Example",organizer:"Naptown Stomp",date:"1989-11-29",location:"Indianapolis, Indiana",price:30.00,event_type:"Workshop",dance_style:"Lindy"})
