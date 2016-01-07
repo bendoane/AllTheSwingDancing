d07ae7b214e30793db10dc004beff470607c31f7
bendoane <ben.doane@gmail.com>
2016-01-07 15:14:48 -0500

    Squashing bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,29 @@
 class UsersController < ApplicationController
 
-def new
-  @user = User.new
-end
+  def new
+    @user = User.new
+  end
 
-def create
-  @user = User.new(user_params)
-  @user.save!
-  redirect_to root_url
-end
+  def create
+    @user = User.new(user_params)
+    @user.save!
+    redirect_to root_url
+  end
 
-def show
-end
+  def show
+  end
 
-def update
-end
+  def update
+  end
 
-def destroy
-  @user = User.find(params[:id])
-end
+  def destroy
+    @user = User.find(params[:id])
+  end
 
 private
 
-def user_params
-  params.require(:user).permit(:username, :email, :password, :password_confirmation)
-end
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 
 end
